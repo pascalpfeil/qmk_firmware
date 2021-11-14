95bf9b1f9451031ac91df285be9cc3d122c89089
Pascal Pfeil <pfeilpascal@outlook.de>
2021-11-14 11:11:56 +0100

    Add underglow to macropad

--- keyboards/3x3macropad/keymaps/default/keymap.c
+++ keyboards/3x3macropad/keymaps/default/keymap.c
@@ -18,7 +18,11 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names
 {
-    _BASE
+    _BASE,
+    _LAYER2,
+    _LAYER3,
+    _LAYER4,
+    LAYER_COUNT
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -27,17 +31,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_A, KC_B, KC_C,
         KC_D, KC_E, KC_F,
         KC_G, KC_H, KC_I),
+
+    [_LAYER2] = LAYOUT(
+        KC_A, KC_B, KC_C,
+        KC_D, KC_E, KC_F,
+        KC_G, KC_H, KC_I),
+
+    [_LAYER3] = LAYOUT(
+        KC_A, KC_B, KC_C,
+        KC_D, KC_E, KC_F,
+        KC_G, KC_H, KC_I),
+
+    [_LAYER4] = LAYOUT(
+        KC_A, KC_B, KC_C,
+        KC_D, KC_E, KC_F,
+        KC_G, KC_H, KC_I),
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise)
 {
+    const uint8_t current_layer = get_highest_layer(layer_state);
     if (clockwise)
     {
-        tap_code(KC_VOLU);
+        layer_move((current_layer + 1) % LAYER_COUNT);
     }
     else
     {
-        tap_code(KC_VOLD);
+        layer_move((current_layer + LAYER_COUNT - 1) % LAYER_COUNT);
     }
     return true;
 }
@@ -51,6 +71,15 @@ void oled_task_user(void)
     {
     case _BASE:
         oled_write_ln_P(PSTR("Base"), false);
+        break;
+    case _LAYER2:
+        oled_write_ln_P(PSTR("Layer2"), false);
+        break;
+    case _LAYER3:
+        oled_write_ln_P(PSTR("Layer3"), false);
+        break;
+    case _LAYER4:
+        oled_write_ln_P(PSTR("Layer4"), false);
         break;
     default:
         oled_write_ln_P(PSTR("Undefined"), false);
